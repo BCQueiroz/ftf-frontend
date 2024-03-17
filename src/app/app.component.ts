991cc82fd31d1553f9062aa1bdb2aea74bc3a7ef
bcqueiroz <bcqueiroz123@gmail.com>
2024-03-17 19:41:50 -0300

    feat: criando a nova aba seletora de telas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponentComponent } from './components/top-bar-component/top-bar-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
