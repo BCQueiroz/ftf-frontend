4e593ad64b2ce099dfd3f1584a3a13c6d5a492f1
bcqueiroz <bcqueiroz123@gmail.com>
2024-04-07 22:47:53 -0300

    feat: criando tela de login, e regras para manter sessão logada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponentComponent } from './components/top-bar-component/top-bar-component.component';
 import { SearchLocalsComponent } from './components/search-locals/search-locals.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginViewComponent } from './components/login-view/login-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponentComponent, SearchLocalsComponent, HttpClientModule],
+  imports: [RouterOutlet, TopBarComponentComponent, SearchLocalsComponent, LoginViewComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginViewComponent } from './components/login-view/login-view.component';
+import { SearchLocalsComponent } from './components/search-locals/search-locals.component';
+import { inject } from '@angular/core';
+import { AuthenticationGuard } from './utils/authentication-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: "login", component: LoginViewComponent },
+    { path: "search-locals", component: SearchLocalsComponent , canActivate: [() => inject(AuthenticationGuard).canActivate()]}
+];
